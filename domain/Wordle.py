b704df2f4838eba5ef05cc2699d9773f11209c4f
Pai <pl9ed15@gmail.com>
2025-11-22 06:24:30 -0500

    updated WordleUser with default values

--- domain/Wordle.py
+++ domain/Wordle.py
@@ -4,7 +4,7 @@ from attr import dataclass
 @dataclass
 class WordleUser:
     name: str
-    win_count: int
+    win_count: int = 0
     # play_count and score_sum used to compute rolling average
-    play_count: int
-    score_sum: int
+    play_count: int = 0
+    score_sum: int = 0
