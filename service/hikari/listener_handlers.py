cd7a68270c05c67d2995cd429b78fb09e8f6ca12
Pai <pl9ed15@gmail.com>
2025-11-22 03:10:34 -0500

    Moved listener handlers to a separate file (#7)
    moved listener handlers to a separate file

---
+++ service/hikari/listener_handlers.py
@@ -0,0 +1,31 @@
+import logging
+from datetime import datetime
+
+from hikari import GuildMessageCreateEvent
+
+from persistence.mongo_client import update_yo_count
+
+logger = logging.getLogger(__name__)
+
+
+async def handle_yo_message(event: GuildMessageCreateEvent):
+    logger.info(f"Received yo from {event.message.author.display_name}")
+    user_count, counter = update_yo_count(event.message.author)
+    total_count = counter['count']
+    threshold: int = counter['threshold']
+    if total_count % threshold == 0:
+        start_date = datetime.fromtimestamp(counter['start_date'])
+        start_date_string = start_date.strftime('%Y-%m-%d')
+        now = datetime.now()
+        total_days = (now - start_date).days
+        average = "{:.2f}".format(counter['count'] / total_days)
+        response = (f"Congrats on being the {counter['count']}th yo! "
+                    f"We've averaged {average} yo's per day since I started counting on {start_date_string}. "
+                    f"You're responsible for at least {user_count}.")
+        await event.message.respond(response)
+
+
+async def handle_wordle_result(event: GuildMessageCreateEvent):
+    logger.info(
+        f"Received wordle result from {event.message.author.display_name}")
+    logger.warning("Not yet implemented")
