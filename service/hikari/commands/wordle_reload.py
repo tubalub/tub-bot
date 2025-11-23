405549af430fe0f04bb1bd07479a6a1799f4331b
Pai <pl9ed15@gmail.com>
2025-11-22 23:00:39 -0500

    added wordle_reload command

---
+++ service/hikari/commands/wordle_reload.py
@@ -0,0 +1,30 @@
+import logging
+
+import lightbulb
+
+from service.hikari.hikari_bot import bot
+from service.hikari.search import search_user_messages
+from service.wordle_service import initialize_wordle_messages
+
+loader = lightbulb.Loader()
+
+logger = logging.getLogger(__name__)
+
+
+@loader.command
+class WordleReload(
+        lightbulb.SlashCommand,
+        name="wordle_reload",
+        description="Force reload wordle scores."):
+
+    @lightbulb.invoke
+    async def invoke(self, context: lightbulb.Context) -> None:
+        logger.info(
+            f"Received command to reload wordle scores from {context.user.username} in {context.channel_id}")
+        await context.defer(ephemeral=True)
+
+        response = await context.respond("Reloading wordle scores. This can take a while.", ephemeral=True)
+
+        await initialize_wordle_messages(bot)
+
+        await context.edit_response(response, "Reloaded wordle scores successfully.")
